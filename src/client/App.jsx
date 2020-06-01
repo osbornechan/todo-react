b637fa10c3e25a05c571fe5175974374197d6ad5
osbornechan <osbornecab@gmail.com>
2020-06-02 02:54:30 +0800

    Able to submit and display tasks

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,10 +2,82 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            currentTask: "",
+            taskId: 0,
+            tasks: [],
+            checked: false
+        }
+    }
+
+    changeHandler(event) {
+        if (event.target.type === 'text') {
+            let newTask = event.target.value;
+
+            this.setState({
+                currentTask: newTask
+            })
+            console.log(this.state.currentTask)
+        } else if (event.target.type === 'checkbox') {
+            if (this.state.checked === false) {
+                this.setState({
+                    checked: true
+                })
+            } else {
+                this.setState({
+                    checked: false
+                })
+            }
+        }
+
+    }
+
+    clickHandler(event){
+        let addTask = this.state.tasks;
+        addTask.push(this.state.currentTask);
+
+        this.setState({
+            tasks: addTask
+        })
+    }
+
   render() {
+
+    const taskList = this.state.tasks.map(task => {
+        return (
+            <label>
+                <input
+                    type='checkbox'
+                    className='check-box'
+                    checked={this.state.checked}
+                    onChange={(event) => {this.changeHandler(event)}}/>
+                {task}
+            </label>
+        )
+    })
+
     return (
-      <div>
-        Welcome.
+      <div className='container'>
+        <h1>You should be doing these..</h1>
+        <h2>
+            <input
+                type='text'
+                className='input'
+                onChange={(event) => {this.changeHandler(event)}}
+                placeholder='Type tasks here..'
+            />
+        </h2>
+        <button
+            className='submit'
+            onClick={()=>{this.clickHandler()}}>
+            Submit
+            </button>
+        <br/>
+        <div className='task-box'>
+            {taskList}
+        </div>
       </div>
     );
   }
